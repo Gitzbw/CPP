18b72586d760cf50675cb9b29e0a21c148640e58
Gitzbw <81919800@qq.com>
2022-07-07 20:44:27 +0800

    test1

---
+++ test1/test1.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 10;
+	char c;
+	//cout << "a = " << a << endl;
+	float f = 3.14123213123123;
+	string str = "abc";
+	char ch[] = "abc";
+	char m[] = {'a','b','\0'};
+	//cout << true << endl;
+	//cin >> c;
+	//cout << c;
+	cout << 5.0 / 1 << "\n";
+	cout << a++ << "\n";
+	cout << ++b << endl;
+	bool flag = false + 1;
+	cout << flag;
+	short x = 20;
+	int y = 30;
+	x > y ? x : y = 10;
+	switch (x)
+	{
+
+	}
+	
+	
+
+
+	return 0;
+}
