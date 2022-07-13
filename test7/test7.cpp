a526137add262a5288d7e83808ed7e06fae84e47
Gitzbw <81919800@qq.com>
2022-07-13 20:46:55 +0800

    test7

---
+++ test7/test7.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	string str = "abcdefg";
+	string subStr = str.substr(1, 3);
+	cout << subStr << endl;
+
+
+
+	return 0;
+}
